28d56a939ce3ac8b46ac64915f9948609a8e75dc
Brandon W Maister <quodlibetor@gmail.com>
2012-01-21 15:26:31 -0500

    Make autojump plugin more system-agnostic
    Use 'type' to make sure that brew is installed before using it,
    and check /etc and /etc/profile.d, which are the default install
    dirs if you run it from install.sh

--- plugins/autojump/autojump.plugin.zsh
+++ plugins/autojump/autojump.plugin.zsh
@@ -1,3 +1,7 @@
-if [ -f `brew --prefix`/etc/autojump ]; then
+if type brew >/dev/null && [ -f `brew --prefix`/etc/autojump ]; then
   . `brew --prefix`/etc/autojump
+elif [ -f /etc/autojump ]; then
+  . /etc/autojump
+elif [ -f /etc/profile.d/autojump.zsh ]; then
+  . /etc/profile.d/autojump.zsh
 fi
